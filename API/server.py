1602f545998f0f748323b8c8edde0678d31d082f
IgorDominguez <igordominguez0079080@gmail.com>
2026-03-29 20:04:09 -0300

    Adicionando a lógica geral da API
    Aqui é onde a API roda, é nessa pasta que temos os códigos principais do servidor da API. Feito em Python também, utilizando FastAPI

---
+++ API/server.py
@@ -0,0 +1,23 @@
+# python -m uvicorn server:app --reload
+
+from fastapi import FastAPI
+from routes.upload_send import router as upload_send_router
+from routes.upload_get import router as upload_get_router
+from routes.upload_resume import router as upload_resume_router
+from routes.upload_clear import router as upload_clear_router
+from routes.chat_send import router as chat_send_router
+from routes.chat_clear import router as chat_clear_router
+from routes.chat_get import router as chat_get_router
+
+app = FastAPI()
+
+# UPLOAD PDF
+app.include_router(upload_send_router)
+app.include_router(upload_get_router)
+app.include_router(upload_resume_router)
+app.include_router(upload_clear_router)
+
+# CHAT
+app.include_router(chat_send_router)
+app.include_router(chat_clear_router)
+app.include_router(chat_get_router)
